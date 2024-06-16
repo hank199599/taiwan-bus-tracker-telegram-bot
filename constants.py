45a22bcc458741afeea37af779c5cdf60204d22b
YU SHAO-HUNG (游紹宏) <hank199599@gmail.com>
2024-06-17 00:06:36 +0800

    feat: add constants file

---
+++ constants.py
@@ -0,0 +1,7 @@
+FIREBASE_CONFIG_PATH = './path/to/firebase/config/file'
+FIREBASE_REALTIME_DB_PATH = 'https://${GCP_PROJECT_NAME}.${GCP_PROJECT_REGION}.firebasedatabase.app/'
+
+TDX_CLIENT_ID = ""
+TDX_CLIENT_SECRET = ""
+
+TELEGRAM_BOT_TOKEN = ''

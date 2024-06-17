e36a21eedc1a77f84cde4ecfdb8b4556a1a266ab
YU SHAO-HUNG (游紹宏) <hank199599@gmail.com>
2024-06-18 01:11:16 +0800

    chore: change main function as pub/sub entrance

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import base64
+import functions_framework
+from cloudevents.http import CloudEvent
 from usecase.notifiyUserUseCase import notifyUserUseCase
 import asyncio
 
-def __main__():
-  usecase = notifyUserUseCase()
-  asyncio.run(usecase.exec())
-
-__main__()
+# Triggered from a message on a Cloud Pub/Sub topic.
+@functions_framework.cloud_event
+def subscribe(cloud_event:CloudEvent) -> None:
+    # Print out the data from Pub/Sub, to prove that it worked
+    print(
+        "Hello, " + base64.b64decode(cloud_event.data["message"]["data"]).decode() + "!"
+    )
+    usecase = notifyUserUseCase()
+    asyncio.run(usecase.exec())
